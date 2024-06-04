cd583fdd18902559472ab6db7843875d9aeb6835
Tarcila Cardoso Alves <t.cardoso.alves@avanade.com>
2024-06-04 15:25:32 -0300

    Departamento: pesquisa e css Update

---
+++ wwwroot/js/pesquisa.js
@@ -0,0 +1,19 @@
+﻿
+
+/* Pesquisa departamento
+
+AJAX permite que você atualize uma parte da página web sem ter que recarregar toda a página.
+
+*/
+$(document).ready(function () {
+    $(".input_search-depart").on("input", function () {
+        var searchValue = $(this).val();
+        $.ajax({
+            url: '@Url.Action("Index", "Departamento")',
+            data: { searchString: searchValue },
+            success: function (data) {
+                $("#departamentoList").html(data);
+            }
+        });
+    });
+});
